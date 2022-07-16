0482efacca83cbc5369f68705719b05b6379b5f8
uroojniazi713 <uroojniazi7@gmail.com>
2022-07-16 19:11:44 +0500

    initial commit

---
+++ src/containers/FeaturePage/index.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react';
+
+export default function FeaturePage() {
+  return (
+    <div>
+
+        <title>Feature Page</title>
+        <meta
+          name="description"
+          content="Feature page of React.js Boilerplate application"
+        />
+    </div>
+  );
+}
---
+++ src/containers/HomePage/index.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react';
+
+export default function HomePage() {
+  return (
+    <div>
+
+        <title>Home Page</title>
+        <meta
+          name="description"
+          content="Home page of React.js Boilerplate application"
+        />
+    </div>
+  );
+}
